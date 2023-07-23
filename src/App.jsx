70a480bc75d8060c4c7a48ed84c4f5edf3861ffc
Anthony Alabado <anthonyalabado3712@gmail.com>
2023-07-24 05:25:41 +0800

    first version

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-// import Intro from "./pages/Intro";
-import AboutMe from "./pages/AboutMe";
-import Skills from "./pages/Skills";
-import Projects from "./pages/Projects";
-import Experience from "./pages/Experience";
-import Footer from "./components/Footer";
-import ContactMe from "./pages/ContactMe";
-import Intro from "./pages/Intro";
-
-import Body from "./components/Body";
-
-//
-//       {/* <AboutMe />
-
 import React from "react";
+import MovieSearch from "./component/MovieSearch";
 
 function App() {
   return (
-    <Body>
-      <Intro />
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <Experience />
-      <ContactMe />
-      <Footer />
-    </Body>
+    <div>
+      <MovieSearch />
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "../src/styles/index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// Remove ReactDOM.render and use createRoot instead
-createRoot(document.getElementById("root")).render(
-  <Router>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </Router>
-);
+  </React.StrictMode>,
+)
